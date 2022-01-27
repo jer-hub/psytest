6c0494c1a3d920dbcfda70cb676d3067701a172b
Jerwin Carreon <jerwin.carreon@jmc.edu.ph>
2022-01-27 08:07:56 +0800

    commit

--- psytests/settings.py
+++ psytests/settings.py
@@ -194,4 +194,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'psytestjmc@gmail.com'
-EMAIL_HOST_PASSWORD = 'Team4titude'
+EMAIL_HOST_PASSWORD = 'kimyrntnphvjvqhg'
