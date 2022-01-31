304d85ed2ce6058bbce84fd2369522d50837b40c
jpf911 <japhet.floren@jmc.edu.ph>
2022-01-31 19:57:27 +0800

    '1/31/2022'

--- psytests/settings.py
+++ psytests/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Falsei
+DEBUG = False
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['jmcproject.herokuapp.com','127.0.0.1', 'localhost']
